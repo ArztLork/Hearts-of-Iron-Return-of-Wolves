e6d09d39c01bd0455486170243b96fa703a9a5ff
FluffyCommando <Gideonstrom@gmail.com>
2025-09-13 05:57:02 -0500

    Change Game End Date

--- Kerberos/common/defines/00_defines.lua
+++ Kerberos/common/defines/00_defines.lua
@@ -1,8 +1,8 @@
 NDefines = {
 
 NGame = {
-	START_DATE = "1936.1.1.12",
-	END_DATE = "1949.1.1.1",
+	START_DATE = "1954.1.1.1",
+	END_DATE = "1975.1.1.1",
 	MAP_SCALE_PIXEL_TO_KM = 7.114,					-- Yes, we did the math
 	SAVE_VERSION = 24,								-- 1.15.0 (unnamed)
 	CHECKSUM_SALT = "zwOdv5d9wm9uDSOT",				-- Data to modify generated checksum when game binaries have changed but not any content files.
